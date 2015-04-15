9de690a1a0031d92bcec9bba903b1d37317ad2f2
Huang Yonggang <huangyg@mars22.com>
2015-04-15 13:41:51 +0800

    NRV

--- food.go
+++ food.go
@@ -16,6 +16,7 @@ type element struct {
 	Name   string
 	Amount float64
 	Unit   string
+	NRV    float64
 }
 
 func fooddaylog(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +36,7 @@ func fooddaylog(w http.ResponseWriter, r *http.Request) {
 			d := dmap[r.Form["date"][0]]
 			log.Print(d.Water)
 
-			water := element{"water", 0, "ml"}
+			water := element{"water", 0, "ml", 0}
 			for _, v := range d.Water {
 				if v.Unit == "ml" {
 					water.Amount = water.Amount + float64(v.Amount)
@@ -44,28 +45,36 @@ func fooddaylog(w http.ResponseWriter, r *http.Request) {
 					water.Amount = water.Amount + float64(1000*v.Amount)
 				}
 			}
+			water.NRV = water.Amount / 2000
 			el.Item = append(el.Item, water)
 
-			emap := make(map[string]float64)
-			vmap := make(map[string]string)
+			emap := make(map[string]float64) //摄入量
+			vmap := make(map[string]string)  //单位
+			nmap := make(map[string]float64) //NRV
 
 			for _, item := range d.Food {
+				log.Printf("FOOD:摄入%s\t %d %s", item.Name, item.Amount, item.Unit)
 				f := fmap[item.Name]
 				a := item.Amount
 
 				for k, v := range f.Element {
+
 					_, ok := emap[k]
 					if ok {
 						emap[k] = float64(a) / float64(f.Amount) * v
+						nmap[k] = float64(a) / float64(f.Amount) * f.NRV[k]
 					} else {
 						emap[k] = emap[k] + float64(a)/float64(f.Amount)*v
+						nmap[k] = nmap[k] + float64(a)/float64(f.Amount)*f.NRV[k]
 					}
 					vmap[k] = f.Unit
+
+					log.Printf("ELEMNT:%s\t %f %s NRV:%f", k, emap[k], vmap[k], nmap[k])
 				}
 			}
 
 			for k, v := range emap {
-				el.Item = append(el.Item, element{k, v, vmap[k]})
+				el.Item = append(el.Item, element{k, v, vmap[k], nmap[k]})
 			}
 
 		}
