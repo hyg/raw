3bf9c53e196c7b7ec52725eecfb30bcd32dc8791
Huang Yonggang <huangyg@mars22.com>
2015-06-27 16:18:33 +0800

    /

--- raw.js
+++ raw.js
@@ -8,7 +8,7 @@ var hmap = new Object();
 var list = fs.readdirSync("food");
 for (var i = 0;i<list.length;i++){
   if(list[i].substr(0,2) == "d."){
-	f = yaml.safeLoad(fs.readFileSync("food\\"+list[i], 'utf8'));
+	f = yaml.safeLoad(fs.readFileSync("food/"+list[i], 'utf8'));
 	fmap[f.date] = f;
   }
 }
@@ -16,7 +16,7 @@ for (var i = 0;i<list.length;i++){
 list = fs.readdirSync("health");
 for (var i = 0;i<list.length;i++){
   if(list[i].substr(0,2) == "d."){
-	h = yaml.safeLoad(fs.readFileSync("health\\"+list[i], 'utf8'));
+	h = yaml.safeLoad(fs.readFileSync("health/"+list[i], 'utf8'));
 	hmap[h.date] = h;
   }
 }
@@ -51,5 +51,5 @@ w2 = w2 + ")";
 
 var wstr = d+"\r\n"+w1+"\r\n"+w2+"\r\nplot(c(1:"+cnt+"),weight1,type=\"b\",pch=15,lty=1,col=\"red\",xaxt=\"n\",xlab = \"date\")\r\nlines(c(1:"+cnt+"),weight2,type=\"b\",pch=17,lty=2,col=\"blue\")\r\nlegend(\"topleft\",inset=.05,title=\"体重曲线\",c(\"睡前\",\"醒后\"),lty=c(1,2),pch=c(15,17),col=c(\"red\",\"blue\"))\r\naxis(1, c(1:"+cnt+"),date)\r\n";
 //console.log(wstr);
-fs.writeFile("health\\weight.R",wstr);
-console.log("\n\n体重曲线在health\\weight.R");
+fs.writeFile("health/weight.R",wstr);
+console.log("\n\n体重曲线在health/weight.R");
