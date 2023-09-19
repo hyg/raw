22a8eb54801a90ee238c86eab107c45dd9eb4687
hyg <huangyg@mars22.com>
2023-09-19 17:24:19 +0800

    sleep

--- raw.js
+++ raw.js
@@ -17,7 +17,7 @@ var etable = new Object();  // element data
 var ftable = new Object();  // food data
 // element detail tables
 //const Keyelement = "热量";
-//const Keyelement = "蛋白质";
+const Keyelement = "蛋白质";
 //const Keyelement = "脂肪";
 //const Keyelement = "碳水化合物";
 //const Keyelement = "钠";
