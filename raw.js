d9dd1a2586c3390700eb649ba8db26ef7277852a
hyg <huangyg@mars22.com>
2024-09-02 06:21:18 +0800

    NRV.202409a.yaml

--- raw.js
+++ raw.js
@@ -11,7 +11,7 @@ var emap = new Object();    // element data
 var hmap = new Object();    // health log
 
 // NRV and DRIs dataset
-var NRVfilename = "food/NRV.202408d.yaml";
+var NRVfilename = "food/NRV.202409a.yaml";
 
 // Statistics period
 var daycnt = 0;
