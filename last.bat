1d9df0be3a0150b9f0ae860c6f8eecb18e5c8909
hyg <huangyg@mars22.com>
2025-10-17 00:24:15 +0800

    day over

--- last.bat
+++ last.bat
@@ -1,7 +1,12 @@
 @echo off
+setlocal enabledelayedexpansion
 
-for /f "tokens=1-4 delims=/ " %%a in ("%date%") do ( set "ymd=%%a%%b%%c")
+:: 获取当前日期（格式：20251015）
+::for /f "tokens=1-4 delims=/ " %%a in ("%date%") do ( set "ymd=%%a%%b%%c")
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value ^| findstr "="') do set "dt=%%a"
+set "ymd=%dt:~0,8%"
 
+::echo %ymd%
 copy food\.last.yaml food\d.%ymd%.yaml
 copy health\.last.yaml health\d.%ymd%.yaml
 edit health\d.%ymd%.yaml
